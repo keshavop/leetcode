6a2d707c441797d149b818312994d5de91cce4ef
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2022-06-14 12:02:34 +0530

    Added solution - LeetHub

---
+++ 5. Coin Change - Minimum number of coins - GFG/5.-coin-change-minimum-number-of-coins.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//Complete this function
+
+class Solution
+{
+    public:
+    //Function to find the minimum number of coins to make the change 
+    //for value using the coins of given denominations.
+    long long minimumNumberOfCoins(int coins[],int numberOfCoins,int value)
+    {
+        vector<int>dp(value+1,0);
+        dp[0]=0;
+        
+        for(int i = 1 ; i <= value ; i++){
+            dp[i] = INT_MAX;
+            for(int j =0 ; j < numberOfCoins; j++){
+                if(i-coins[j] >=0 && dp[i-coins[j]]!=INT_MAX ){
+                    dp[i] = min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[value]==INT_MAX ? -1 : dp[value];
+    }
+};
+
+// { Driver Code Starts.
+
+
+
+int main() {
+	
+	//taking total count of testcases
+	int testcases;
+	cin>>testcases;
+	while(testcases--)
+	{
+	    //taking value
+	    int value;
+	    cin>>value;
+	    
+	    //taking number of coins
+	    int numberOfCoins;
+	    cin>>numberOfCoins;
+	    int coins[numberOfCoins];
+	    
+	    //taking value of each coin
+	    for(int i=0;i<numberOfCoins;i++)
+	    cin>>coins[i];
+	    Solution obj;
+	    //calling function minimumNumberOfCoins()
+	    int answer=obj.minimumNumberOfCoins(coins,numberOfCoins,value);
+	    
+	    //printing "Not Possible" if answer is -1
+	    //else printing answer
+	    if(answer==-1)
+	    cout<<"Not Possible"<<endl;
+	    else
+	    cout<<answer<<endl;
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
