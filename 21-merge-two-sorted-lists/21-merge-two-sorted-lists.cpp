bf013524b754dfdb1415195c2c080807c4cb41fd
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2022-08-09 00:35:37 +0530

    Time: 3 ms (98.44%), Space: 14.8 MB (42.46%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,50 +11,50 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
-            if (a == NULL)
-    {
-        return b;
-    }
-    if (b == NULL)
-    {
-        return a;
-    }
-    ListNode *head = NULL;
-    ListNode *tail = NULL;
-    if (a->val <= b->val)
-    {
-        head = tail = a;
-        a = a->next;
-    }
-    else
-    {
-        head=tail=b;
-        b = b->next;
-    }
-
-    while (a != NULL && b != NULL)
-    {
+                if (a == NULL)
+        {
+            return b;
+        }
+        if (b == NULL)
+        {
+            return a;
+        }
+        ListNode *head = NULL;
+        ListNode *tail = NULL;
         if (a->val <= b->val)
         {
-            tail->next = a;
-            tail = a;
+            head = tail = a;
             a = a->next;
         }
         else
         {
-            tail->next = b;
-            tail = b;
+            head=tail=b;
             b = b->next;
         }
-    }
-    if (a == NULL)
-    {
-        tail->next = b;
-    }
-    else
-    {
-        tail->next = a;
-    }
-    return head;
-    }
+
+        while (a != NULL && b != NULL)
+        {
+            if (a->val <= b->val)
+            {
+                tail->next = a;
+                tail = a;
+                a = a->next;
+            }
+            else
+            {
+                tail->next = b;
+                tail = b;
+                b = b->next;
+            }
+        }
+        if (a == NULL)
+        {
+            tail->next = b;
+        }
+        else
+        {
+            tail->next = a;
+        }
+        return head;
+        }
 };
