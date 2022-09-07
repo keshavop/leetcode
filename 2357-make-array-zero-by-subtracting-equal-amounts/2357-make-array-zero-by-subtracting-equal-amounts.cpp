e492182f861d8e7fc08336823d6f020a88a2c808
Keshav Kumar <71931145+keshavop@users.noreply.github.com>
2022-09-07 16:25:51 +0530

    Time: 10 ms (13.71%), Space: 8.5 MB (35.39%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,14 @@
+// Trick -> no of move = no of unique elements in vector
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto i:nums){
+            if(i != 0){
+                s.insert(i);
+            }
+        }
+    return s.size();
+    }
+};
